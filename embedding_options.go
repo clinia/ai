232e262963a8672811fbcf816789e037e3102319
Brando Tovar <brando.tovar@clinia.com>
2025-08-14 09:15:15 -0400

    feat: allow url override on embedding call

--- embedding_options.go
+++ embedding_options.go
@@ -23,6 +23,24 @@ func WithEmbeddingHeaders[T any](headers http.Header) EmbeddingOption[T] {
 	}
 }
 
+// WithEmbeddingProviderMetadata sets provider-specific metadata for the embedding call.
+func WithEmbeddingProviderMetadata[T any](provider string, metadata any) EmbeddingOption[T] {
+	return func(o *EmbeddingOptions[T]) {
+		if o.EmbeddingOptions.ProviderMetadata == nil {
+			o.EmbeddingOptions.ProviderMetadata = api.NewProviderMetadata(map[string]any{})
+		}
+		o.EmbeddingOptions.ProviderMetadata.Set(provider, metadata)
+	}
+}
+
+// WithEmbeddingBaseURL sets the base URL for the embedding API endpoint.
+func WithEmbeddingBaseURL[T any](baseURL string) EmbeddingOption[T] {
+	url := baseURL
+	return func(o *EmbeddingOptions[T]) {
+		o.EmbeddingOptions.BaseURL = &url
+	}
+}
+
 // WithEmbeddingEmbeddingOptions sets the entire api.EmbeddingOptions struct.
 func WithEmbeddingEmbeddingOptions[T any](embeddingOptions api.EmbeddingOptions) EmbeddingOption[T] {
 	return func(o *EmbeddingOptions[T]) {

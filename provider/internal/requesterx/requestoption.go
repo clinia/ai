481e3cd960c4d2573be1384484bef6679fff2e73
Khalil-Clinia <khalil.krayem@clinia.com>
2025-10-30 15:24:53 -0400

    creating common definitions

---
+++ provider/internal/requesterx/requestoption.go
@@ -0,0 +1,72 @@
+package requesterx
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func WithBaseURL(raw string) RequestOption {
+	return RequestOptionFunc(func(r *RequestConfig) error {
+		u, err := url.Parse(raw)
+		if err != nil {
+			return err
+		}
+		r.BaseURL = u
+		return nil
+	})
+}
+
+// WithHeader returns a RequestOption that sets the header value to the associated key. It overwrites
+// any value if there was one already present.
+func WithHeader(key, value string) RequestOption {
+	return RequestOptionFunc(func(r *RequestConfig) error {
+		r.Request.Header.Set(key, value)
+		return nil
+	})
+}
+
+// WithHeaderAdd returns a RequestOption that adds the header value to the associated key. It appends
+// onto any existing values.
+func WithHeaderAdd(key, value string) RequestOption {
+	return RequestOptionFunc(func(r *RequestConfig) error {
+		r.Request.Header.Add(key, value)
+		return nil
+	})
+}
+
+// WithHeaderDel returns a RequestOption that deletes the header value(s) associated with the given key.
+func WithHeaderDel(key string) RequestOption {
+	return RequestOptionFunc(func(r *RequestConfig) error {
+		r.Request.Header.Del(key)
+		return nil
+	})
+}
+
+// WithHTTPClient returns a RequestOption that changes the underlying http client used to make this
+// request, which by default is [http.DefaultClient].
+func WithHTTPClient(c *http.Client) RequestOption {
+	return RequestOptionFunc(func(r *RequestConfig) error {
+		if c != nil {
+			r.HTTPClient = c
+		}
+		return nil
+	})
+}
+
+// WithAPIKey returns a RequestOption that sets the client setting "api_key".
+func WithAPIKey(value string) RequestOption {
+	return RequestOptionFunc(func(r *RequestConfig) error {
+		r.APIKey = value
+		return r.Apply(WithHeader("authorization", fmt.Sprintf("Bearer %s", r.APIKey)))
+	})
+}
+
+// WithUseRawBaseURL instructs the client to use the configured BaseURL as the
+// full request URL without appending a path from the request.
+func WithUseRawBaseURL() RequestOption {
+	return RequestOptionFunc(func(r *RequestConfig) error {
+		r.UseRawBaseURL = true
+		return nil
+	})
+}

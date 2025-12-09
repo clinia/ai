cf3f67f1742a691d4d48eccdb666a4bc12093f6e
Khalil-Clinia <khalil.krayem@clinia.com>
2025-12-09 12:45:42 -0500

    chore: refactoring

--- examples/basic/tei-ranking/main.go
+++ examples/basic/tei-ranking/main.go
@@ -50,7 +50,7 @@ func main() {
 	// Example 1: Using the standard API-compliant RankingModel interface
 	fmt.Println("=== Example 1: API-Compliant Ranking (DoRank) ===")
 
-	rankingModel, err := provider.RankingModel("BAAI/bge-reranker-large")
+	rankingModel, err := provider.RankingModel("")
 	if err != nil {
 		log.Fatalf("Failed to create ranking model: %v", err)
 	}
